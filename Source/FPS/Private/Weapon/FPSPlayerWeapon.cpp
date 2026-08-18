b2dd119c0525669eecc46d1418cea43477f2d84e
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-08-19 00:31:23 +0530

    Setup Player Weapon

---
+++ Source/FPS/Private/Weapon/FPSPlayerWeapon.cpp
@@ -0,0 +1,30 @@
+﻿// No Copyright.
+
+#include "Weapon/FPSPlayerWeapon.h"
+
+AFPSPlayerWeapon::AFPSPlayerWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true;
+	
+	WeaponMesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh1PComp"));
+	SetRootComponent(WeaponMesh1P);
+	WeaponMesh1P->SetHiddenInGame(true);
+	WeaponMesh1P->SetCastShadow(false);
+	WeaponMesh1P->SetReceivesDecals(false);
+	WeaponMesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	
+	WeaponMesh3P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh3PComp"));
+	WeaponMesh3P->SetupAttachment(GetRootComponent());
+	WeaponMesh3P->SetHiddenInGame(true);
+	WeaponMesh3P->SetCastShadow(true);
+	WeaponMesh3P->SetReceivesDecals(false);
+	WeaponMesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+}
+
+void AFPSPlayerWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/FPS/Public/Weapon/FPSPlayerWeapon.h
@@ -0,0 +1,32 @@
+﻿// No Copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSPlayerWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AFPSPlayerWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFPSPlayerWeapon();
+	
+	FORCEINLINE USkeletalMeshComponent* GetWeaponMesh1P() const { return WeaponMesh1P; }
+	FORCEINLINE USkeletalMeshComponent* GetWeaponMesh3P() const { return WeaponMesh3P; }
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category = "FPS|Mesh")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh1P;
+	
+	UPROPERTY(VisibleAnywhere, Category = "FPS|Mesh")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh3P;
+};
