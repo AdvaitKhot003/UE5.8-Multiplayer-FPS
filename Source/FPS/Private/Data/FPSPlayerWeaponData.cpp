cbc716734765f2dca22e0bbaf4b8444873d6aeb7
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-08-19 01:54:44 +0530

    Setup Player Weapon Data

---
+++ Source/FPS/Private/Data/FPSPlayerWeaponData.cpp
@@ -0,0 +1,4 @@
+﻿// No Copyright.
+
+#include "Data/FPSPlayerWeaponData.h"
+
---
+++ Source/FPS/Public/Data/FPSPlayerWeaponData.h
@@ -0,0 +1,21 @@
+﻿// No Copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "FPSPlayerWeaponData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API UFPSPlayerWeaponData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "FPS|Weapon", meta = (Categories = "Weapon.WeaponType"))
+	TMap<FGameplayTag, FName> WeaponGripPoints;
+};
